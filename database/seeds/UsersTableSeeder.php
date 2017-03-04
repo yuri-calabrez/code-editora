d9addd3bdc57e9a3413fc279280c468abf7ce35e
Yuri Calabrez <yuri.calabrez@gmail.com>
2017-03-04 00:12:08 -0300

    Commit inicial

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        factory(\App\User::class, 1)->create([
+            'name' => 'Yuri',
+            'email' => 'yuri.calabrez@gmail.com',
+            'password' => bcrypt('123456')
+        ]);
+    }
+}
